4e7eb090a8085e2abe5cdefd7dcf77d338ff757c
PHUTAWAN SASTRAKOM <phutawan_sa@cmu.ac.th>
2024-09-03 15:56:47 +0700

    "update feature1"

--- Labgit/Game1.cs
+++ Labgit/Game1.cs
@@ -42,7 +42,7 @@ namespace Labgit
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Pink);
 
             // TODO: Add your drawing code here
 
